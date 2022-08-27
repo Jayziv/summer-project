a35d590dc1c2334cfd3cb90e847aeed7252821c8
phesodge <phesodge@gmail.com>
2022-08-27 09:36:44 +0000

    git changes

--- configs/user.js
+++ configs/user.js
@@ -1,4 +1,4 @@
-//Copy this to .gitignore then enter your details TODO: add /contact details
+//Copy this to ../gitignore/ then enter your details TODO: add /contact details
 export const user = {
     userName: 'Your Name',
     occupation: 'Your Title',
