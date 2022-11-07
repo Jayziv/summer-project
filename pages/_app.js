b6ce569cab51da7da939d4a871635b8950f92cb6
Jayface <jaynicolsmith@gmail.com>
2022-11-07 20:18:36 +0000

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,30 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { Analytics } from '@vercel/analytics/react';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from '../lib/ga'
 
 function MyApp({ Component, pageProps }) {
+
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            ga.pageview(url)
+        }
+        //When the component is mounted, subscribe to router changes
+        //and log those page views
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        //If the component is unmounted, unsubscribe
+        //from the event with the `off` method
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+    
   return (
     <Layout>
       <Component {...pageProps} />
