ccc936e903981e62d67b67c89d0a5e3821a493b9
Jayface <jaynicolsmith@gmail.com>
2022-11-19 12:11:46 +0000

    animation works - red background - doesn't seem best

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,34 @@
 import Link from "next/link";
 import Image from "next/image";
 import { gsap } from "gsap";
+import { CSSRulePlugin } from "gsap/dist/CSSRulePlugin";
 import Logo from ".//../images/JNS-Logo.svg";
 import { useLayoutEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
-export default function Layout({ children }) {
+gsap.registerPlugin(CSSRulePlugin);
+
+export default function Layout() {
   useLayoutEffect(() => {
-    gsap.to("#v-shapes", { x: 200 })
-  
-    return () => {
-      
-    };
-  }, [])
+    const leftV = CSSRulePlugin.getRule("#vshape::before");
+    const rightV = "test";
+    gsap.to(leftV, {
+      backgroundColor: "#000",
+    });
+
+    console.log(leftV);
+    console.log(rightV);
+
+    return () => {};
+  }, []);
 
   return (
     <div>
-      <div id="v-shape" className="absolute block h-screen w-full overflow-hidden text-blue-400 before:absolute before:block before:h-screen before:w-full before:origin-top-left before:rotate-45 before:border before:bg-slate-400 after:absolute after:block after:h-screen after:w-full after:origin-top-right after:-rotate-45 after:border after:bg-slate-400 before:pointer-events-none after:pointer-events-none"></div>
+      <div
+        id="vshape"
+        className="vshape absolute block h-screen w-full overflow-hidden text-blue-400 before:pointer-events-none before:absolute before:block before:h-screen before:w-full before:origin-top-left before:rotate-45 before:border before:bg-slate-400 after:pointer-events-none after:absolute after:block after:h-screen after:w-full after:origin-top-right after:-rotate-45 after:border after:bg-slate-400"
+      ></div>
       <div className="container mx-auto text-white">
         <div className="">
           <div className="self-center">
@@ -30,7 +41,11 @@ export default function Layout({ children }) {
           <h2 className="text-center text-4xl font-thin leading-6">Digital.</h2>
         </div>
 
-        <div ><p className="my-4 text-center font-semibold text-white thetween">eqwe</p></div>
+        <div>
+          <p className="thetween my-4 text-center font-semibold text-white">
+            eqwe
+          </p>
+        </div>
       </div>
     </div>
   );
