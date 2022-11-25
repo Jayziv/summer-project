10ed7ec4c33d7772536270840711db187f079736
Jayface <jaynicolsmith@gmail.com>
2022-11-25 02:19:52 +0000

    progress on positioning of vShapes and process.browser to run in browser only - is deprecated - further research needed

--- components/Content.js
+++ components/Content.js
@@ -3,21 +3,28 @@ import Logo from ".//../images/JNS-Logo.svg";
 
 function Content(props) {
   return (
-    <div className="container mx-auto text-white">
-      <div id="glasses" className="mt-12 mb-8 flex justify-center">
-        <Image className="" src={Logo} alt="Logo" width={140} height={80} />
+    <div className="container relative mx-auto text-white">
+      <div id="glasses" className="mx-auto mt-12 mb-8 flex justify-center">
+        <Image className="" src={Logo} alt="Logo" className="logo" />
       </div>
-      <div id="hero-content">
-        <h2 className="mt-6 text-center text-7xl font-semibold leading-8">
+      <div id="hero-content" className="relative">
+        <h2 className="header-content mt-6 text-center text-7xl font-semibold leading-8">
           Developer.
         </h2>
-        <h2 className="text-center text-6xl italic leading-tight">Design.</h2>
-        <h2 className="text-center text-5xl font-thin leading-6">Digital.</h2>
+        <h2 className="header-content text-center text-6xl italic leading-tight">
+          Design.
+        </h2>
+        <h2 className="header-content text-center text-5xl font-thin leading-6">
+          Digital.
+        </h2>
       </div>
 
       <div className="flex-center my-10 flex w-auto justify-center">
-        <p id="btn-mellon" className="btn-mellon hover:x-4 my-4 w-auto border px-4 py-2 text-center font-semibold italic text-white hover:cursor-pointer">
-          mellon
+        <p
+          id="btn-stare"
+          className="btn-stare hover:x-4 my-4 w-auto border px-4 py-2 text-center font-semibold italic text-white hover:cursor-pointer"
+        >
+          its rude to stare
         </p>
       </div>
     </div>
--- components/Layout.js
+++ components/Layout.js
@@ -9,72 +9,109 @@ import Footer from "./Footer";
 gsap.registerPlugin(CSSRulePlugin);
 
 export default function Layout() {
-  useLayoutEffect(() => {
-  const lShape = "#v-shape-l";
-  const rShape = "#v-shape-r";
-  const diamondShape = "header .navigation-links";
-  const heroContent = "#hero-content";
-  var button = typeof document !== 'undefined' && document.querySelector("#btn-mellon");
+  if (process.browser) {
+    var marginTarget = document.getElementById("shapes-container");
+    var style =
+      marginTarget.currentStyle || window.getComputedStyle(marginTarget);
 
-  function curtains() {
-    let tl = gsap.timeline({ defaults: { ease: "back", duration: 2 } });
-    tl.to(
-      lShape,
-      {
-        x: -500,
-      },
-    );
-    tl.to(
-      rShape,
-      {
-        x: 500,
-      },
-      "<"
-    );
-    tl.fromTo(
-      diamondShape,
-      {
-        
-        y: 300,
-      },
-      {
-        delay: .23,
-        y: 0,
-        stagger: {
-          each: 0.1,
-          from: "center",
-        },
-      },
-      "<"
-    );
-    tl.fromTo(
-      heroContent,
-      {
-        x: 30,
-      },
-      {
-        x: 0,
-        opacity:1,
-      },
-    );
-    // build scene 1
-    return tl;
+    window.addEventListener("resize", function () {
+      let maxWidth = style.getPropertyValue("max-width");
+      console.log(maxWidth);
+    });
   }
 
-  let master = gsap.timeline();
-  master.add(curtains());
-  master.pause();
-   
-  button.addEventListener("click", function(){
-    master.play();
-  });
+  useLayoutEffect(() => {
+    const lShape = "#v-shape-l";
+    const rShape = "#v-shape-r";
+    const diamondShape = "header .navigation-links";
+    const heroContent = ".header-content";
+    var buttonID = "#btn-stare";
+    var button =
+      typeof document !== "undefined" && document.querySelector(buttonID);
+
+    // function fadeIn() {
+    //   gsap.to(lShape, { duration: 0.5, x: 0, opacity: 1 });
+    //   gsap.to(rShape, { duration: 0.5, x: 0, opacity: 1 });
+    //   gsap.to(diamondShape, { duration: 0.5, y: 0, opacity: 1 });
+    //   gsap.to(heroContent, { duration: 0.5, y: 0, opacity: 1 });
+
+    //   return tl;
+    // }
+
+    function curtains() {
+      let tl = gsap.timeline({ defaults: { ease: "back", duration: 1.55 } });
+      tl.to(buttonID, {
+        duration: 1.2,
+        display: "none",
+        opacity: 0,
+        y: 100,
+      });
+      tl.to(heroContent, {
+        duration: 0.1,
+        display: "block",
+      });
+      tl.to(lShape, {
+        x: -500,
+      });
+      tl.to(
+        rShape,
+        {
+          x: 500,
+          delay: 0.15,
+        },
+        "<"
+      );
+      tl.fromTo(
+        diamondShape,
+        {
+          y: 300,
+        },
+        {
+          delay: 0.23,
+          y: 0,
+          stagger: {
+            each: 0.1,
+            from: "center",
+          },
+        }
+      );
+      tl.fromTo(
+        heroContent,
+        {
+          x: 30,
+        },
+        {
+          x: 0,
+          opacity: 1,
+          stagger: {
+            each: 0.26,
+            from: "start",
+          },
+        },
+        "1"
+      );
+
+      // build scene 1
+      return tl;
+    }
+
+    let master = gsap.timeline();
+    master.add(curtains());
+    master.pause();
+
+    button.addEventListener("click", function () {
+      master.play();
+    });
 
     return () => {};
   }, []);
 
   return (
-    <div className="home-container">
-      <div className="shapes-container z-50">
+    <div className="home-container relative" suppressHydrationWarning={true}>
+      <div
+        id="shapes-container"
+        className="shapes-container container pointer-events-none absolute left-1/2 z-50 flex -translate-x-1/2 justify-center"
+      >
         <div
           id="v-shape-l"
           className="vshape jns-card pointer-events-none absolute block origin-top-left rotate-45 overflow-hidden text-blue-400 drop-shadow"
